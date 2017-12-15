46b421b9f3f376b4737f7fab6d8436d57d637cce
liuzhuang <liuzhuangthu@gmail.com>
2017-12-15 23:01:36 +0800

    create repository, add source files

---
+++ config.py
@@ -0,0 +1,42 @@
+from easydict import EasyDict as edict
+# construct parameters for different method
+SD_param = edict()
+ND_param = edict()
+
+# decide if SD is driven by data
+SD_param['data_driven'] = False
+# FLOPs compression ratio for each layer
+SD_param['c_ratio'] = 3
+# enable trigger for SD
+SD_param['enable'] = False
+
+# threshold of energy ratio for network decoupling
+ND_param['energy_threshold'] = 0.9
+# enable trigger for ND
+ND_param['enable'] = True
+
+# dict containg layers not requiring spatial decomposition
+mask_layers = ['conv1'] + ['res%da_branch2a' % i for i in range(3,6)] + ['res%da_brach2b' % i for i in range(3,6)] + ['res%da_brach1' % i for i in range(3,6)]
+
+# gpu device (-1 for CPU)
+device_id = 0
+
+# parameters for data driven decoupling
+# the input layer name of network
+data_layer = 'data'
+# the dataset used for data reconstruction
+dataset = 'imagenet'
+# samples of batches for data reconstruction
+nSamples = 500
+# extract how many points per sample
+nPointsPerSample = 10
+# accurate or mAP layer names for data driven method (default value is accuracy@5 in vgg-16)
+accname = 'acc/top-5'
+# the name of frozen pickle to store sample points
+frozen_name = 'frozen'
+# test param
+caffe_path = '/home/jli59/yuxili/ker2col-caffe/build/tools/caffe'
+# imagenet val source
+imagenet_val = '/data/sde/jli59/jianbo/lmdb/ilsvrc12_train_lmdb'
+# cifar10 val source
+cifar10_val = '/path/to/imagenet_val'
